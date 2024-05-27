ca8fc16d8f6d8647d28010b037416d3a7761ee0c
Юлия <118264321+Morozova91@users.noreply.github.com>
2024-05-27 21:36:50 +0300

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,27 @@
+# Функции.
+# Задача "Матрица воплоти"
+#Объявите функцию get_matrix и напишите в ней параметры n, m и value.
+#Создайте пустой список matrix внутри функции get_matrix.
+#Напишите первый(внешний) цикл for для кол-ва строк матрицы, n повторов.
+#В первом цикле добавляйте пустой список в список matrix.
+#Напишите второй(внутренний) цикл for для кол-ва столбцов матрицы, m повторов.
+#Во втором цикле пополняйте ранее добавленный пустой список значениеми value.
+#После всех циклов верните значение переменной matrix.
+#Выведите на экран(консоль) результат работы функции get_ma
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        empty_list = []
+        matrix.append(empty_list)
+        for j in range(m):
+            matrix[i].append(value)
+
+    return matrix
+
+
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
